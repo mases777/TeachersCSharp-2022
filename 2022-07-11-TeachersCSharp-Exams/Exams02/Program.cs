229f7e849b91aca09bce96daa4325d0714a22ed9
mases777 <mases@abv.bg>
2022-07-21 20:41:49 +0300

    Exams02

---
+++ 2022-07-11-TeachersCSharp-Exams/Exams02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+char[] separator = new char[] { ' ', '.' };
+Console.WriteLine("Въведете изречение, което съдържа няколко думи, разделени с интервал и завършващо с . (точка)");
+List<string> sentence = Console.ReadLine()
+    .Split(separator, StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+Console.WriteLine($"Броят на думите в изречението е: {sentence.Count}");
+
+var words = new Dictionary<string, int>();
+foreach (var item in sentence)
+{
+    if (!words.ContainsKey(item))
+    {
+        words.Add(item, item.Length);
+    }
+}
+int maxWordLength = words.Select(x => x.Value).Max();
+string maxWord = words.Where(x => x.Value == maxWordLength).Select(x => x.Key).First();
+
+Console.WriteLine($"Най-дългата дума е: {maxWord}");
