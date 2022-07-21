cceba6887cfb673204ae92be09b13fcc4d054b5e
mases777 <mases@abv.bg>
2022-07-21 20:51:29 +0300

    Exams04

---
+++ 2022-07-11-TeachersCSharp-Exams/Exams04/Program.cs
@@ -0,0 +1,19 @@
+﻿List<int> oddNumbers = new List<int>();
+List<int> evenNumbers = new List<int>();
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Въведете цяло число: ");
+    int number = int.Parse(Console.ReadLine());
+    if (number % 2 == 0)
+    {
+        evenNumbers.Add(number);
+    }
+    else
+    {
+        oddNumbers.Add(number);
+    }
+}
+
+Console.WriteLine($"Сумата на четните числа е: {evenNumbers.Sum()}");
+Console.WriteLine($"Сумата на нечетните числа е: {oddNumbers.Sum()}");
