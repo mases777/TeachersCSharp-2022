31e556fb30e87492fa5fdd549c39b3cc4e858275
mases777 <mases@abv.bg>
2022-07-21 12:03:06 +0300

    Exams04

---
+++ 2022-07-07-TeachersCSharp-Exams/Exams04/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Въведете дължина на страна на равнобедрен триъгълник: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write(new String(' ', n));
+Console.WriteLine("*");
+for (int i = 0; i < n - 1; i++)
+{
+    Console.Write(new String(' ', n - i - 1));
+    Console.Write("*");
+    Console.Write(new String(' ', 2 * i + 1));
+    Console.WriteLine("*");
+}
+Console.WriteLine(new String('*', 2 * n + 1));
