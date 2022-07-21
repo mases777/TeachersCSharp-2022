5a8ddcd36a36a058faec46f2cbb2bcf33892fd53
mases777 <mases@abv.bg>
2022-07-21 11:28:11 +0300

    exams03

---
+++ 2022-07-06-TeachersCSharp-Exams/Exams03/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Въведете марка на колата: ");
+string? car = Console.ReadLine();
+Console.Write("Въведете цвят на колата: ");
+string? color = Console.ReadLine();
+Console.Write("Въведете разход на 100 км: ");
+string? liter = Console.ReadLine();
+Console.Write("Въведете марка на гумите: ");
+string? tyre = Console.ReadLine();
+
+Console.WriteLine($"Колата на Петър е {car}, {color} на цвят, разход {liter} nа сто, а гумите й са марка {tyre}");
