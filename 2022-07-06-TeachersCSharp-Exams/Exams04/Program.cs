70304fcdd58797ef0b3877fa386abfbde3e2f4a2
mases777 <mases@abv.bg>
2022-07-21 11:34:19 +0300

    Exams04

---
+++ 2022-07-06-TeachersCSharp-Exams/Exams04/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Въведете първото число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Въведете второто число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Въведете третото число: ");
+int c = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Средноаритметично на трите числа е: {(a + b + c) / 3.0}");
