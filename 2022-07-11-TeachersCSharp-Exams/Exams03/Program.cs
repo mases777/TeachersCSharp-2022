95027eb3d15c461b84380b656db90fd193b544bd
mases777 <mases@abv.bg>
2022-07-21 20:46:18 +0300

    Exams03

---
+++ 2022-07-11-TeachersCSharp-Exams/Exams03/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Задача за пресмятане на степен.");
+Console.Write("Въведете число за основа: ");
+double baseNumber = double.Parse(Console.ReadLine());
+Console.Write("Въведете число за степенен показател: ");
+double powerNumber = double.Parse(Console.ReadLine());
+
+Console.WriteLine(Math.Pow(baseNumber, powerNumber));
