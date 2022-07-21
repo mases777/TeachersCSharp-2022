8ed02ccd0fb7581d20b2d4f6075eba4e3df85e4f
mases777 <mases@abv.bg>
2022-07-21 20:59:58 +0300

    Exams05

---
+++ 2022-07-11-TeachersCSharp-Exams/Exams05/Program.cs
@@ -0,0 +1,13 @@
+﻿List<int> numbers = new List<int>();
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Въведете цяло число: ");
+    numbers.Add(int.Parse(Console.ReadLine()));
+}
+var averageNumber = numbers.Average();
+
+int lowNumbers = numbers.Where(x => x < averageNumber).Count();
+int highNumbers = numbers.Where(x => x > averageNumber).Count();
+
+Console.WriteLine($"Броят на числата, които са по-малки от средната стойност са: {lowNumbers}");
+Console.WriteLine($"Броят на числата, които са по-големи от средната стойност са: {highNumbers}");
