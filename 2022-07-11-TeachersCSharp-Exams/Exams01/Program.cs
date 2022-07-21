7ebf76ca1954335862a4487ef99c3cf8e1b3f860
mases777 <mases@abv.bg>
2022-07-21 20:09:21 +0300

    Exams01

---
+++ 2022-07-11-TeachersCSharp-Exams/Exams01/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Въведете Хс на окръжността: ");
+double xc = double.Parse(Console.ReadLine());
+Console.Write("Въведете Yс на окръжността: ");
+double yc = double.Parse(Console.ReadLine());
+Console.Write("Въведете радиуса на окръжността: ");
+double radius = double.Parse(Console.ReadLine());
+Console.Write("Въведете Хt на дадената точка: ");
+double xt = double.Parse(Console.ReadLine());
+Console.Write("Въведете Yt на дадената точка: ");
+double yt = double.Parse(Console.ReadLine());
+
+double cathA = Math.Abs(xc - xt);
+double cathB = Math.Abs(yc - yt);
+double hypotenuse = Math.Sqrt(cathA * cathA + cathB * cathB);
+
+if (radius > hypotenuse)
+{
+    Console.WriteLine("Точката е вътреша за окръжността.");
+}
+else if (radius < hypotenuse)
+{
+    Console.WriteLine("Точката е външна за окръжността.");
+}
+else
+{
+    Console.WriteLine("Точката лежи на окръжността.");
+}
