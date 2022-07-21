fde7377cd8370264f2c347db44bf38055ef3ddb0
mases777 <mases@abv.bg>
2022-07-21 11:16:30 +0300

    exams02

---
+++ 2022-07-06-TeachersCSharp-Exams/Exams02/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Въведете радиус на окръжност: ");
+double r = double.Parse(Console.ReadLine());
+double perimeter = 2 * r * Math.PI;
+double area = r * r * Math.PI;
+Console.WriteLine($"Лицето на окръжността е: {area:f2}");
+Console.WriteLine($"Периметърът на окръжността е: {perimeter:f2}");
