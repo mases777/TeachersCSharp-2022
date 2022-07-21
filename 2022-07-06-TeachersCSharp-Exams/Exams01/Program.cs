c0ab1ed9ed75c1ba46609250baaa353d05b86bd7
Mehmed Ali Mehmed <mases@abv.bg>
2022-07-21 11:09:51 +0300

    Add files via upload

---
+++ 2022-07-06-TeachersCSharp-Exams/Exams01/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Въведете страна на квардат: ");
+double a = double.Parse(Console.ReadLine());
+double perimeter = 4 * a;
+double area = a * a * Math.PI;
+Console.WriteLine($"Лицето на квадрата е: {area:f2}");
+Console.WriteLine($"Периметърът на квадрата е: {perimeter:f2}");
