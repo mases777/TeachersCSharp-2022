93818b5a9cb57856f7f9daf47ada8e3560318890
mases777 <mases@abv.bg>
2022-07-24 12:36:17 +0300

    Exams01

---
+++ 2022-07-18-TeachersCSharp-Exams/Exams01/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Въведете масив от числа разделени със запетая и интервал");
+char[] separator = new char[] { ' ', ',' };
+var array = Console.ReadLine()
+    .Split(separator, StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse).ToArray();
+var dict = new Dictionary<int, int>();
+foreach (var item in array)
+{
+    if (!dict.ContainsKey(item))
+    {
+        dict.Add(item, 1);
+    }
+    else
+    {
+        dict[item]++;
+    }
+}
+Console.WriteLine($"Най-често срещания елемент е: {dict.OrderByDescending(x => x.Value).Select(x => x.Key).FirstOrDefault()}");
