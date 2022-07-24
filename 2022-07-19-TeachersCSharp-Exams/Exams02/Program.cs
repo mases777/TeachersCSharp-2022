b161c59741cb818d2c2a0fbb07578cd3cd2d23b7
mases777 <mases@abv.bg>
2022-07-24 13:38:10 +0300

    Exams02

---
+++ 2022-07-19-TeachersCSharp-Exams/Exams02/Program.cs
@@ -0,0 +1,22 @@
+﻿List<int> list = new List<int>();
+while (true)
+{
+    Console.Write("Въведете поредица от числа, разделени с интервал: ");
+    list = Console.ReadLine().Split().Select(int.Parse).ToList();
+    if (list.Count > 0) break;
+}
+Console.WriteLine($"Първото число, който е по-голям от двата свои съседи едновременно е на позиция: {BigElement(list)}");
+
+static int BigElement(List<int> list)
+{    
+    int bigIndex = 0;
+    for (int i = 1; i < list.Count - 1; i++)
+    {
+        if (list[i] > list[i - 1] && list[i] > list[i + 1])
+        {            
+            bigIndex = i; break;
+        }
+    }
+    if (bigIndex == 0) return -1;
+    return bigIndex;
+}
