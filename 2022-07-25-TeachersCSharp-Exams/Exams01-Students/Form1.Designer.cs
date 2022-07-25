22ed2bf466a62831fccf6914f62e85d4c8788d21
mases777 <mases@abv.bg>
2022-07-25 19:40:39 +0300

    Exams01

---
+++ 2022-07-25-TeachersCSharp-Exams/Exams01-Students/Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace Exams01_Students
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBName = new System.Windows.Forms.TextBox();
+            this.comBBEL = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.comBMat = new System.Windows.Forms.ComboBox();
+            this.comBFiz = new System.Windows.Forms.ComboBox();
+            this.comBHim = new System.Windows.Forms.ComboBox();
+            this.comBBio = new System.Windows.Forms.ComboBox();
+            this.comBInf = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(149, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(519, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Информационна система \"Students\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Файл";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(330, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = ".txt";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(84, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 34);
+            this.textBox1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.comBInf);
+            this.panel1.Controls.Add(this.comBBio);
+            this.panel1.Controls.Add(this.comBHim);
+            this.panel1.Controls.Add(this.comBFiz);
+            this.panel1.Controls.Add(this.comBMat);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.comBBEL);
+            this.panel1.Controls.Add(this.textBName);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(13, 135);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(446, 298);
+            this.panel1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(13, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 28);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ученик:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(183, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 28);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Номер в класа";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(13, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 28);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Име";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(13, 127);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 28);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "БЕЛ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(355, 23);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 34);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // textBName
+            // 
+            this.textBName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBName.Location = new System.Drawing.Point(84, 71);
+            this.textBName.Name = "textBName";
+            this.textBName.Size = new System.Drawing.Size(339, 34);
+            this.textBName.TabIndex = 9;
+            // 
+            // comBBEL
+            // 
+            this.comBBEL.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.comBBEL.FormattingEnabled = true;
+            this.comBBEL.Location = new System.Drawing.Point(84, 127);
+            this.comBBEL.Name = "comBBEL";
+            this.comBBEL.Size = new System.Drawing.Size(53, 36);
+            this.comBBEL.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(163, 127);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 28);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Мат";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(312, 127);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 28);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Физ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(13, 181);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(54, 28);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Хим";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(164, 181);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(50, 28);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "Био";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label12.Location = new System.Drawing.Point(311, 181);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(56, 28);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "Инф";
+            // 
+            // comBMat
+            // 
+            this.comBMat.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.comBMat.FormattingEnabled = true;
+            this.comBMat.Location = new System.Drawing.Point(227, 127);
+            this.comBMat.Name = "comBMat";
+            this.comBMat.Size = new System.Drawing.Size(53, 36);
+            this.comBMat.TabIndex = 16;
+            // 
+            // comBFiz
+            // 
+            this.comBFiz.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.comBFiz.FormattingEnabled = true;
+            this.comBFiz.Location = new System.Drawing.Point(370, 127);
+            this.comBFiz.Name = "comBFiz";
+            this.comBFiz.Size = new System.Drawing.Size(53, 36);
+            this.comBFiz.TabIndex = 17;
+            // 
+            // comBHim
+            // 
+            this.comBHim.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.comBHim.FormattingEnabled = true;
+            this.comBHim.Location = new System.Drawing.Point(84, 178);
+            this.comBHim.Name = "comBHim";
+            this.comBHim.Size = new System.Drawing.Size(53, 36);
+            this.comBHim.TabIndex = 18;
+            // 
+            // comBBio
+            // 
+            this.comBBio.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.comBBio.FormattingEnabled = true;
+            this.comBBio.Location = new System.Drawing.Point(227, 178);
+            this.comBBio.Name = "comBBio";
+            this.comBBio.Size = new System.Drawing.Size(53, 36);
+            this.comBBio.TabIndex = 19;
+            // 
+            // comBInf
+            // 
+            this.comBInf.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.comBInf.FormattingEnabled = true;
+            this.comBInf.Location = new System.Drawing.Point(370, 178);
+            this.comBInf.Name = "comBInf";
+            this.comBInf.Size = new System.Drawing.Size(53, 36);
+            this.comBInf.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(395, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Създай";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Window;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(550, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Отвори";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Window;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(705, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 34);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Запиши";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Window;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(14, 241);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 34);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Съхрани";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(474, 135);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(355, 298);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 457);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Students";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Panel panel1;
+        private Button button4;
+        private ComboBox comBInf;
+        private ComboBox comBBio;
+        private ComboBox comBHim;
+        private ComboBox comBFiz;
+        private ComboBox comBMat;
+        private Label label12;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private ComboBox comBBEL;
+        private TextBox textBName;
+        private TextBox textBox2;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ 2022-07-25-TeachersCSharp-Exams/Exams01-Students/Form1.cs
@@ -0,0 +1,97 @@
+namespace Exams01_Students
+{
+    public partial class Form1 : Form
+    {
+        string[] a = new string[32];
+        int fileRows, rtbCount;
+        string[] rEl = new string[8];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void parseRec(string record)
+        {
+            int comma, next;
+            comma = record.IndexOf(','); 
+            rEl[0] = record.Substring(0, comma);
+            for (int i = 1; i < 7; i++)
+            {
+                next = record.IndexOf(',', comma + 1);
+                rEl[i] = record.Substring(comma + 1, next - comma - 1);
+                comma = next;
+            }
+            rEl[7] = record.Substring(comma + 1);
+            textBName.Text = rEl[1];
+            comBBEL.Text = rEl[2]; comBMat.Text = rEl[3];
+            comBFiz.Text = rEl[4]; comBHim.Text = rEl[5];
+            comBBio.Text = rEl[6]; comBInf.Text = rEl[7];
+        }
+        private void cleanRec()
+        {
+            textBox2.Text = "";
+            textBName.Text = "";
+            comBBEL.Text = "";
+            comBMat.Text = "";
+            comBFiz.Text = "";
+            comBHim.Text = "";
+            comBBio.Text = "";
+            comBInf.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fileName = textBox1.Text;
+            StreamReader r = new StreamReader(fileName + ".txt");
+            richTextBox1.Text = r.ReadToEnd();
+            //чете файла
+            // ако потребителят не е въвел нов ред в края
+            rtbCount = fileRows = richTextBox1.Lines.Count();
+            if (richTextBox1.Lines[rtbCount - 1] == "") fileRows--;
+            else richTextBox1.Text += "\n";
+            // Създваме редактируемото копие  
+            for (int i = 0; i < fileRows; i++)
+            { a[i + 1] = richTextBox1.Lines[i]; }
+            r.Close();
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox2.Text);
+            if (i > 0 && i <= fileRows)
+                parseRec(a[i]);
+            else cleanRec();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            cleanRec();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox2.Text);
+            if (i < 0 || i > fileRows)
+            { fileRows++; i = fileRows; }
+            string newRec = fileRows.ToString() + "," + textBName.Text +
+            "," + comBBEL.Text + "," + comBMat.Text + "," + comBFiz.Text +
+            "," + comBHim.Text + "," + comBBio.Text + "," + comBInf.Text;
+            a[i] = newRec;
+            textBox2.Text = fileRows.ToString();
+            richTextBox1.Text = "";
+            for (int j = 1; j <= fileRows; j++) richTextBox1.Text += (a[j] + "\n");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StreamWriter w = new StreamWriter(textBox1.Text + ".txt", true);
+            for (int i = 1; i <= fileRows; i++) w.WriteLine(a[i]);
+            w.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fileRows = 0; richTextBox1.Clear();
+        }
+    }
+}
